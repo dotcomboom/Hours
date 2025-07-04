73e36ccade43e257b80594340fa9e502454c17a3
dcb <38927017+dotcomboom@users.noreply.github.com>
2025-07-04 04:04:13 -0500

    Overhaul interface, overall simplification

--- Hours/My Project/AssemblyInfo.vb
+++ Hours/My Project/AssemblyInfo.vb
@@ -11,14 +11,14 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyTitle("Hours")> 
 <Assembly: AssemblyDescription("")> 
 <Assembly: AssemblyCompany("dotcomboom")> 
-<Assembly: AssemblyProduct("Hours")> 
-<Assembly: AssemblyCopyright("Copyright © dotcomboom 2024")> 
+<Assembly: AssemblyProduct("Hours")>
+<Assembly: AssemblyCopyright("Copyright © dotcomboom 2025")>
 <Assembly: AssemblyTrademark("")> 
 
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("c6a29daf-b71d-4f14-8da2-775236ec60d4")> 
+<Assembly: Guid("c6a29daf-b71d-4f14-8da2-775236ec60d4")>
 
 ' Version information for an assembly consists of the following four values:
 '
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.0")> 
-<Assembly: AssemblyFileVersion("1.0.0.0")> 
+<Assembly: AssemblyVersion("2.0.0.0")>
+<Assembly: AssemblyFileVersion("2.0.0.0")>
--- Hours/frmWipFeatures.vb
+++
@@ -1,3 +0,0 @@
-﻿Public Class frmWipFeatures
-
-End Class
